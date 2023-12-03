09efd1b97f12259c0b82927bf9745863bc078ca0
NGUYEN NGOC QUY <112646774+NguyenQuy1306@users.noreply.github.com>
2023-12-03 08:09:13 +0700

    Delete DB2-main directory

--- DB2-main/BTL 2 databse file/2.thủ tục cho bảng taikhoandangnhap/Precedure_nguoithamgiachuyenbay_delete.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE PROCEDURE DeleteNguoiThamGiaChuyenBay
-    @MaKhachHang VARCHAR(255)
-AS
-BEGIN
-    -- Thực hiện xóa dữ liệu
-    DELETE FROM NguoiThamGiaChuyenBay
-    WHERE MaKhachHang = @MaKhachHang;
-END;
