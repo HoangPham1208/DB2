37ea96f564f355b17e875c3d8001f878b80e3cf0
NGUYEN NGOC QUY <112646774+NguyenQuy1306@users.noreply.github.com>
2023-12-08 14:04:11 +0700

    Update Step_4_Proce.sql

--- DB-forShowinginSQLserver/Step_4_Proce.sql
+++ DB-forShowinginSQLserver/Step_4_Proce.sql
@@ -35,14 +35,15 @@ VALUES
 VALUES
   ( 'Le Van C', '0901234567', 'lecv@gmail.com', '111122223333', '1995-03-08', 'A002', 'CB002', 'Economy');
 
-CREATE OR ALTER PROCEDURE TimPhongKhachSan (@Date DATE, @City VARCHAR(20))
+CREATE OR ALTER PROCEDURE TimPhongKhachSan (@DateCheckIn DATE,@DateCheckOut DATE, @City VARCHAR(20))
 AS
 BEGIN
-   SELECT K.TenKhachSan, P.LoaiPhong, P.SoLuongCungCap, P.GiaPhong
+   declare @period INT=DATEDIFF(DAY, @DateCheckIn, @DateCheckOut); 
+   SELECT P.MaSoThueKhachSan, K.TenKhachSan, P.LoaiPhong, P.GiaPhong,min(P.SoLuongCungCap) as SoLuongCungCap
    FROM KhachSan K, Phong P
-   WHERE K.MaSoThue = P.MaSoThueKhachSan 
-   and P.Ngay = @Date 
-   and K.ThanhPho = @City
+   WHERE K.MaSoThue = P.MaSoThueKhachSan and P.Ngay >= @DateCheckIn AND  P.Ngay <= @DateCheckOut and K.ThanhPho = @City
+   GROUP BY  P.MaSoThueKhachSan, K.TenKhachSan, P.LoaiPhong, P.GiaPhong
+   having count(*)> @period
    ORDER BY P.GiaPhong ASC
 END
 
