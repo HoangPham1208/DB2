8c9f42e7749525605d64b667c5b9d8b4f89b0e69
Trinhvanhan <han.trinhthbd082k3@hcmut.edu.vn>
2023-12-08 10:58:16 +0700

    Update Step_4_Proce.sql

--- DB-forShowinginSQLserver/Step_4_Proce.sql
+++ DB-forShowinginSQLserver/Step_4_Proce.sql
@@ -1,17 +1,24 @@
-CREATE OR ALTER PROCEDURE SoluongNguoiBayTheoNgay @Date DATE
+CREATE OR ALTER PROCEDURE SoluongNguoiBayTheoNgay @Date DATE, @Start VARCHAR(20), @End VARCHAR(20)
 AS
 BEGIN
-   SELECT C.MaSo, K.LoaiKhoang, COUNT(*) AS SoLuongGhe
-   FROM Chuyenbay C, KhoangTrenChuyenBay K, NguoiThamGiaChuyenBay N
-   WHERE CONVERT(DATE, C.ThoiGianXuatPhat) = @Date 
-      and C.MaSo = K.MaSoMayBay 
-      and K.MaSoMayBay = N.MaSoMayBay 
-      and K.LoaiKhoang = N.LoaiKhoang
-   GROUP BY C.MaSo, K.LoaiKhoang
-   ORDER BY SoLuongGhe DESC
+   SELECT K.MaSoMayBay, K.LoaiKhoang, K.SoLuongGheToiDaCungCap, MAX(K.SoLuongGheToiDaCungCap) - COUNT(N.HoVaTen) AS SoLuongGheConLai
+   FROM Chuyenbay C JOIN KhoangTrenChuyenBay K ON C.MaSo = K.MaSoMayBay 
+		LEFT JOIN NguoiThamGiaChuyenBay N ON (K.MaSoMayBay = N.MaSoMayBay and K.LoaiKhoang = N.LoaiKhoang)
+ 
+	WHERE CONVERT(DATE, C.ThoiGianXuatPhat) = @Date 
+		  and C.DiaDiemXuatPhat = @Start
+		  and C.DiaDiemHaCanh = @End
+   GROUP BY K.MaSoMayBay, K.LoaiKhoang, K.SoLuongGheToiDaCungCap
+   HAVING MAX(K.SoLuongGheToiDaCungCap) - COUNT(N.HoVaTen) > 0
+   ORDER BY SoLuongGheConLai
 END
+GO
 
--- exec SoluongNguoiBayTheoNgay @Date = '2023-03-02'
+
+exec SoluongNguoiBayTheoNgay @Date = '2023-03-02', @End = 'HaNoi', @Start = 'Ho Chi Minh City'
+GO
+
+select * from NguoiThamGiaChuyenBay
 GO
 
 CREATE OR ALTER PROCEDURE TimPhongKhachSan (@Date DATE, @City VARCHAR(20))
