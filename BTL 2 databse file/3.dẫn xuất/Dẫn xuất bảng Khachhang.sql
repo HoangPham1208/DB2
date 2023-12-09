d7bff505f4adfd64e161949d52f27857e2209e69
TheBowl65t <147915647+TheBowl65t@users.noreply.github.com>
2023-12-09 22:44:30 +0700

    Update Dẫn xuất bảng Khachhang.sql

--- BTL 2 databse file/3.dẫn xuất/Dẫn xuất bảng Khachhang.sql
+++ BTL 2 databse file/3.dẫn xuất/Dẫn xuất bảng Khachhang.sql
@@ -1,20 +1,20 @@
 -- Create a function for calculating the total amount for a KhachHang
-CREATE FUNCTION dbo.CalculateTotalAmountPaid(@MaKhachHang VARCHAR(20))
+CREATE FUNCTION dbo.CalculateTotalAmountPaid(@MaSoTaiKhoan VARCHAR(20))
 RETURNS INT
 AS
 BEGIN
     DECLARE @TotalAmount INT;
 
     SELECT @TotalAmount = SUM(C.GiaKhoang)
-    FROM VeMayBay AS a
-    JOIN NguoiThamGiaChuyenBay AS b ON a.MaVe = b.MaVeMayBay
-    JOIN KhoangChuyenBay AS c ON b.MaSoMayBay = c.MaSoMayBay AND b.LoaiKhoang = c.LoaiKhoang
+    FROM VeDatMayBay AS a
+    JOIN NguoiThamGiaChuyenBay AS b ON a.MaDatVe = b.MaVeMayBay
+    JOIN KhoangTrenChuyenBay AS c ON b.MaSoMayBay = c.MaSoMayBay AND b.LoaiKhoang = c.LoaiKhoang
     JOIN DonHang AS d ON a.MaDonHang = d.MaDonHang
-    WHERE d.MaKhachHang = @MaKhachHang AND d.TinhTrangDonHang='ThanhCong'
+    WHERE d.MaKhachHang = @MaSoTaiKhoan AND d.TinhTrangDonHang='ThanhCong'
     GROUP BY d.MaKhachHang;
     RETURN ISNULL(@TotalAmount,0);
 END;
 
 -- Add a computed column for TongTien in the KhachHang table
 ALTER TABLE KhachHang
-ADD TienThanhThanhToan AS dbo.CalculateTotalAmountPaid(MaKhachHang);
+ADD TienThanhThanhToan AS dbo.CalculateTotalAmountPaid(MaSoTaiKhoan);
