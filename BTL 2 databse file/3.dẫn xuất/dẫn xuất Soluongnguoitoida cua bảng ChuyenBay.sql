b41099034da7d86bbc83d5501d58af6524b6fab5
TheBowl65t <147915647+TheBowl65t@users.noreply.github.com>
2023-12-09 22:47:36 +0700

    Update dẫn xuất Soluongnguoitoida cua bảng ChuyenBay.sql

--- BTL 2 databse file/3.dẫn xuất/dẫn xuất Soluongnguoitoida cua bảng ChuyenBay.sql
+++ BTL 2 databse file/3.dẫn xuất/dẫn xuất Soluongnguoitoida cua bảng ChuyenBay.sql
@@ -1,4 +1,3 @@
--- Create the function for calculating the remaining seat amount
 CREATE FUNCTION dbo.CalculateTotalAmountInChuyenBay(@MaSoMayBay VARCHAR(20))
 RETURNS INT
 AS
@@ -6,13 +5,12 @@ BEGIN
     DECLARE @TotalAmount INT;
     SELECT @TotalAmount = SUM(b.SoLuongGheToiDaCungCap)
     FROM ChuyenBay AS a
-    JOIN KhoangChuyenBay AS b ON a.MaSo = b.MaSoMayBay 
+    JOIN KhoangTrenChuyenBay AS b ON a.MaSo = b.MaSoMayBay 
     WHERE a.MaSo = @MaSoMayBay
     GROUP BY a.MaSo;
     RETURN ISNULL(@TotalAmount, 0);
 END;
 
--- Add a computed column for SoLuongGheConLai in the KhoangChuyenBay table
 ALTER TABLE ChuyenBay
 ADD SoLuongNguoiToiDa AS dbo.CalculateTotalAmountInChuyenBay(MaSo);
 
