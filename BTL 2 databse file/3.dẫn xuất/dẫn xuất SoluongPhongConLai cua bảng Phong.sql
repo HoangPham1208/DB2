9556bf4921fcd40188ebb82c964885156bcbadc6
NGUYEN NGOC QUY <112646774+NguyenQuy1306@users.noreply.github.com>
2023-12-10 21:41:43 +0700

    Update dẫn xuất SoluongPhongConLai cua bảng Phong.sql

--- BTL 2 databse file/3.dẫn xuất/dẫn xuất SoluongPhongConLai cua bảng Phong.sql
+++ BTL 2 databse file/3.dẫn xuất/dẫn xuất SoluongPhongConLai cua bảng Phong.sql
@@ -1,18 +1,14 @@
--- Create the function for calculating the remaining seat amount
 CREATE FUNCTION dbo.CalculateToTalLeftAmountPhong(@MaSoThueKhachSan VARCHAR(20),@LoaiPhong VARCHAR(20),@Ngay DATE)
 RETURNS INT
 AS
 BEGIN
     DECLARE @TotalLeftAmount INT;
-    SELECT @TotalLeftAmount = MAX(a.SoLuongCungCap) - COUNT(*)
-    FROM Phong AS a
+    SELECT @TotalLeftAmount = ISNULL(a.SoLuongCungCap, 0) - ISNULL(b.SoLuong, 0)
+    FROM Phong AS a left join ChonPhong as b on a.MaSoThueKhachSan=b.MaSoThueKhachSan AND a.LoaiPhong=b.LoaiPhong AND a.Ngay=b.Ngay
     WHERE a.MaSoThueKhachSan = @MaSoThueKhachSan AND a.LoaiPhong=@LoaiPhong AND a.Ngay=@Ngay
-    GROUP BY a.MaSoThueKhachSan, a.LoaiPhong,a.Ngay;
     RETURN ISNULL(@TotalLeftAmount, 0);
 END;
 
 -- Add a computed column for SoLuongGheConLai in the KhoangChuyenBay table
 ALTER TABLE Phong
 ADD SoLuongPhongConLai AS dbo.CalculateToTalLeftAmountPhong(MaSoThueKhachSan, LoaiPhong,Ngay);
-
-select * from Phong
