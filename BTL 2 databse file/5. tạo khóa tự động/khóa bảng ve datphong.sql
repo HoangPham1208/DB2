0ef1b216fdeffa3608a89f40131f6bf690942658
NGUYEN NGOC QUY <112646774+NguyenQuy1306@users.noreply.github.com>
2023-12-11 19:34:40 +0700

    Update khóa bảng ve datphong.sql

--- BTL 2 databse file/5. tạo khóa tự động/khóa bảng ve datphong.sql
+++ BTL 2 databse file/5. tạo khóa tự động/khóa bảng ve datphong.sql
@@ -1,18 +1,18 @@
-﻿CREATE TRIGGER instead_of_insert_VeDatPhong
-ON VeDatPhong
-INSTEAD OF INSERT
+CREATE OR ALTER PROCEDURE InsertAndGetAutoKey_VeDatPhong
+(
+    @MaDonHang VARCHAR(20)
+)
 AS
 BEGIN
-    -- Custom logic to generate MaSo and handle the insert
-    DECLARE @nextID INT;
+     DECLARE @nextID INT;
 
     -- Lấy giá trị mã số tăng dần tiếp theo cho mỗi hàng
     SELECT @nextID = COALESCE(MAX(CAST(SUBSTRING(MaDatPhong, 4, LEN(MaDatPhong) - 3) AS INT)), 0) + ROW_NUMBER() OVER (ORDER BY (SELECT NULL))
     FROM VeDatPhong;
 
+	SELECT 'VDP'  + RIGHT('000' + CAST(@nextID AS VARCHAR(3)), 3)
+
     -- Chèn dữ liệu mới và cập nhật MaSo
     INSERT INTO VeDatPhong (MaDonHang,MaDatPhong)
-    SELECT MaDonHang, 'VDP'  + RIGHT('000' + CAST(@nextID AS VARCHAR(3)), 3)
-    FROM INSERTED;
+    VALUES( @MaDonHang, 'VDP'  + RIGHT('000' + CAST(@nextID AS VARCHAR(3)), 3));
 END;
-
